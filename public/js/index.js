d581e2f60ce612bc2479a78e0772c4529a98cb94
Brad Maibach <bmaibach@gmail.com>
2018-07-07 13:37:53 -0700

    Added emitters and listeners for new messages

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Connected to server');
+    socket.emit('createMessage', {
+        from:'jeff@something.com',
+        text:"From a form, typically"
+    });
+});
+
+
+
+socket.on('disconnect', function(){
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message){
+    console.log('New message received', message)
+});
+
--- server/server.js
+++ server/server.js
@@ -15,8 +15,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from:'mike',
+        text:"Hey what's up",
+        createdAt:123123123
+    });
+
     socket.on('disconnect', (socket) => {
         console.log('Client disconnected');
+    });
+
+    socket.on('createMessage', (createdMessage) => {
+        console.log('createEmail', createdMessage)
     });
 });
 
