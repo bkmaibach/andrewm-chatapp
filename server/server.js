7236b05eca59f0aa32e5e59b413a4fd9fbc3c41a
Brad Maibach <bmaibach@gmail.com>
2018-07-07 13:06:57 -0700

    Added basic socket.io server and client side connect and disconnect listeners

--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public', );
 
 const app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', (socket) => {
+        console.log('Client disconnected');
+    });
+});
+
+
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Chat App server is up, listening on port ${port}`)
 });
